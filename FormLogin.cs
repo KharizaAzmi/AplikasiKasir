3a1a8b14e986dad15efbb1907070238af887ef01
Khariza <kharizaazmi@gmail.com>
2023-03-10 06:52:28 +0700

    Add project files.

---
+++ FormLogin.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplikasiKasir
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "ADM001" && textBox2.Text == "admin123")
+            {
+                FormMenuUtama formMenu = new FormMenuUtama();
+                formMenu.ShowDialog();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("username/password salah");
+            }
+        }
+    }
+}
